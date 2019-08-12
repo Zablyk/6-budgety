d844a79214fcb67ada6dcad19775d2e33573e317
Oleg ZAblotskyi <zablotsky@gmail.com>
2019-08-12 18:25:59 -0400

    Add curent month and year + changesd colours for input fields

--- app.js
+++ app.js
@@ -184,7 +184,32 @@ var UIController = (function() {
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
         expensesPercLabel: '.item__percentage'
-    }
+    };
+    
+    var formatNumber = function(number, type) {
+        var numSplit, int, dec, type;
+        /*
+        + or - before number
+        exectly 2 decimal points
+        comma separating the thousands
+        
+        2310.4572 -> + 2,310.46
+        2000 -> + 2,000.00 
+        */
+        num = Math.abs(number);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+
+        int = numSplit[0];
+        if (int.length > 3) {
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); //input 23510 output 23,510
+        }
+
+        dec = numSplit[1];
+        return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+    };
+    
     
     return{
         getInput: function() {
@@ -213,7 +238,7 @@ var UIController = (function() {
             
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
             
             // Insert HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml); 
@@ -240,11 +265,13 @@ var UIController = (function() {
         },
         
         displayBudget: function(obj) {
-            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalIncome;
-            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExpenses;
-            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+            var type;
+            obj.budget > 0 ? type = 'inc' : type = 'exp';
 
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalIncome, 'inc');
+            document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExpenses, 'exp');
+           
             if (obj.percentage > 0) {
                 document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';                         
             } else {
@@ -268,8 +295,8 @@ var UIController = (function() {
                 current.textContent = '---';
             }
             });
-
         },
+
 
         getDOMstrings: function() {
         return DOMstrings; 
